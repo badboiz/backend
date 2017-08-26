29f48bc4b5da060fef93fdd3fc15db9c8c9a07d5
justinvos <justinvosnz@gmail.com>
2017-08-26 19:47:49 +1200

    Added a folder creation section to the gulp ts task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-var gulp = require("gulp")
-var ts = require('gulp-typescript')
+const gulp = require("gulp")
+const ts = require('gulp-typescript')
+const fs = require('fs')
+
+const outputDir = './dist'
+
+
+
+
 
 gulp.task("ts", [], function() {
+  if(!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir)
+  }
+
   gulp.src("server/**/*.ts")
     .pipe(ts({
       moduleResolution: "node",
       noImplicitAny: false
-        }))
-    .pipe(gulp.dest("dist"))
+    }))
+    .pipe(gulp.dest(outputDir))
 })
 
 gulp.task("ts-watch", [], function() {
