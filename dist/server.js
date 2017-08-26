15a67d67733b729f291610eea0cbb122a0263348
justinvos <justinvosnz@gmail.com>
2017-08-26 13:41:14 +1200

    Added test entry point

--- dist/server.js
+++ dist/server.js
@@ -1,3 +1,9 @@
 var express = require('express');
 var app = express();
+app.get('/api/test', function (req, res) {
+    res.json(JSON.stringify({
+        error: false
+    }));
+});
+app.listen(3000);
 console.log('Running');
--- server/server.ts
+++ server/server.ts
@@ -2,4 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.get('/api/test', function(req, res) {
+	res.json(JSON.stringify({
+		error: false
+	}))
+})
+
+app.listen(3000)
 console.log('Running')
