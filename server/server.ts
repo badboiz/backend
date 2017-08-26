741aeb30f1e652b30bc94d6cdc17353cdceceae6
Steven <kan02100@naver.com>
2017-08-26 18:01:35 +1200

    removed w.controller

--- server/server.ts
+++ server/server.ts
@@ -10,3 +10,16 @@ app.get('/api/test', function(req, res) {
 
 app.listen(process.env.PORT || 3000)
 console.log('Running')
+
+
+var getLatLong = () =>{
+		lat:lat,
+		long:long
+	}, (error, response, body)=>{
+		if (!error && response.statusCode ==200){
+			controller.checkUsersSurround(lat,long);
+		} else{
+			console.log('Unable to fetch lat, long');
+	}
+	})
+}
