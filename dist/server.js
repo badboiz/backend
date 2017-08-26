b145c8ad3965fd33b1b3c11ac9e1558cfbe3e83c
justinvos <justinvosnz@gmail.com>
2017-08-26 15:49:57 +1200

    Added CORS ovveride

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 function hasParameters(query, params) {
